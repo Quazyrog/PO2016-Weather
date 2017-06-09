69985ebb42fe910be2eb3b0cef62fbeead4bd28a
Wojciech Matusiak <wm382710@mimuw.edu.pl>
2017-06-09 20:28:58 +0200

    Maven was added to project (so I hope)

--- src/main/java/com/github/saphir2357/po2016/weather/data/IWeatherUpdate.java
+++ src/main/java/com/github/saphir2357/po2016/weather/data/IWeatherUpdate.java
@@ -1,7 +1,6 @@
 package com.github.saphir2357.po2016.weather.data;
 
 public interface IWeatherUpdate {
-    boolean isReady();
     boolean hasData(WeatherUpdateDataKey data);
     double get(WeatherUpdateDataKey data);
 }
